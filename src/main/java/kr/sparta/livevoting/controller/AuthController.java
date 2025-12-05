48a1051704cd87ec6bf57ae711a109b308dc591a
B-ChJ <bchaejoo@gmail.com>
2025-12-05 17:13:42 +0900

    fix: 공통 응답 삭제 fix: Spring Security 전체 접속 허용

--- src/main/java/kr/sparta/livevoting/controller/AuthController.java
+++ src/main/java/kr/sparta/livevoting/controller/AuthController.java
@@ -1,6 +1,5 @@
 package kr.sparta.livevoting.controller;
 
-import kr.sparta.livevoting.dto.CommonResponse;
 import kr.sparta.livevoting.dto.auth.LoginRequest;
 import kr.sparta.livevoting.dto.auth.SignUpRequest;
 import kr.sparta.livevoting.dto.auth.SignUpResponse;
@@ -21,24 +20,16 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/signup")
-    public ResponseEntity<CommonResponse<SignUpResponse>> signup(@RequestBody SignUpRequest request) {
+    public ResponseEntity<SignUpResponse> signup(@RequestBody SignUpRequest request) {
         SignUpResponse response = authService.register(request);
 
-        CommonResponse<SignUpResponse> body = CommonResponse.<SignUpResponse>builder()
-                .data(response)
-                .build();
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(body);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<CommonResponse<TokenResponse>> login(@RequestBody LoginRequest request) {
+    public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest request) {
         TokenResponse token = authService.login(request);
 
-        CommonResponse<TokenResponse> body = CommonResponse.<TokenResponse>builder()
-                .data(token)
-                .build();
-
-        return ResponseEntity.status(HttpStatus.OK).body(body);
+        return ResponseEntity.status(HttpStatus.OK).body(token);
     }
 }
--- src/main/java/kr/sparta/livevoting/controller/VoteController.java
+++ src/main/java/kr/sparta/livevoting/controller/VoteController.java
@@ -1,6 +1,5 @@
 package kr.sparta.livevoting.controller;
 
-import kr.sparta.livevoting.dto.CommonResponse;
 import kr.sparta.livevoting.dto.vote.VoteInfoResponse;
 import kr.sparta.livevoting.dto.vote.VoteRequest;
 import kr.sparta.livevoting.dto.vote.VoteResponse;
@@ -19,24 +18,16 @@ public class VoteController {
     private final VoteService voteService;
 
     @PostMapping
-    public ResponseEntity<CommonResponse<VoteResponse>> createVote(@RequestBody VoteRequest request) {
+    public ResponseEntity<VoteResponse> createVote(@RequestBody VoteRequest request) {
         VoteResponse response = voteService.create(request);
 
-        CommonResponse<VoteResponse> body = CommonResponse.<VoteResponse>builder()
-                .data(response)
-                .build();
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(body);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping
-    public ResponseEntity<CommonResponse<List<VoteInfoResponse>>> getVotes() {
+    public ResponseEntity<List<VoteInfoResponse>> getVotes() {
         List<VoteInfoResponse> votes = voteService.getVotes();
 
-        CommonResponse<List<VoteInfoResponse>> body = CommonResponse.<List<VoteInfoResponse>>builder()
-                .data(votes)
-                .build();
-
-        return ResponseEntity.status(HttpStatus.OK).body(body);
+        return ResponseEntity.status(HttpStatus.OK).body(votes);
     }
 }
